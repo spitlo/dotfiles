4a3167bf34e16c2cebe50fd62954964978329bb8
Jakob Bergman <jakob@aino.se>
2013-07-10 12:01:47 +0200

    For safekeepoing

--- bin/cdp.sh
+++ bin/cdp.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
+if [[ -z "$PROJECT_FOLDER" ]]; then
+    echo "You have not set your project folder in .extras. Exiting."
+else
+    path1=$PROJECT_FOLDER/$1/src/$1
+    path2=$PROJECT_FOLDER/$1/$1
+    path3=$PROJECT_FOLDER/$1
 
-path1=~/Projects/$1/src/$1
-path2=~/Projects/$1/$1
-path3=~/Projects/$1
-path4=$PROJECT_FOLDER/$1
-
-if [ -d $path1 ]; then
-     cd $path1
-     pwd
-elif [ -d $path2 ]; then
-    cd $path2
-    pwd
-elif [ -d $path3 ]; then
-    cd $path3
-    pwd
-elif [ -d $path4 ]; then
-    cd $path4
-    pwd
-fi
+    if [ -d $path1 ]; then
+         cd $path1
+         pwd
+    elif [ -d $path2 ]; then
+        cd $path2
+        pwd
+    elif [ -d $path3 ]; then
+        cd $path3
+        pwd
+    fi
 
 
-if [ -e ~/.virtualenvs/$1/bin/activate ]; then
-    source /usr/local/bin/virtualenvwrapper.sh
-    workon $1
-elif [ "$VIRTUAL_ENV" != "" ]; then
-    source /usr/local/bin/virtualenvwrapper.sh
-    deactivate
+    if [ -e ~/.virtualenvs/$1/bin/activate ]; then
+        source /usr/local/bin/virtualenvwrapper.sh
+        workon $1
+    elif [ "$VIRTUAL_ENV" != "" ]; then
+        source /usr/local/bin/virtualenvwrapper.sh
+        deactivate
+    fi
 fi
